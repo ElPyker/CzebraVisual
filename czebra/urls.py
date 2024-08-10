8aeb6984bf8dccbc8f13282955149e18357f654b
ElPyker <elpyker@gmail.com>
2024-08-10 02:58:53 -0700

    frontend

--- czebra/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/users/', include('Users.urls')),  # Incluye las URLs de la app Users
-    path('api/requests/', include('Requests.urls')),  # Incluye las URLs de la app Requests
-]
